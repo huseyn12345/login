92ec0220ceef5a9881844915a03f3426d49b6305
huseyn12345 <147710134+huseyn12345@users.noreply.github.com>
2025-10-01 22:09:41 +0400

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,126 +1,69 @@
-﻿namespace WinFormsApp1
+using System;
+using System.Windows.Forms;
+
+namespace MPL3
 {
-    partial class Form1
+    public partial class Form1 : Form
     {
-        private System.ComponentModel.IContainer components = null;
+        private string currentOperator = "";
+        private double firstNumber = 0;
+        private bool isOperatorClicked = false;
 
-        protected override void Dispose(bool disposing)
+        public Form1()
         {
-            if (disposing && (components != null))
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (txtDisplay.Text == "0" || isOperatorClicked)
+                txtDisplay.Text = "";
+            isOperatorClicked = false;
+            txtDisplay.Text += button.Text;
+        }
+
+        private void Operator_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            currentOperator = button.Text;
+            firstNumber = double.Parse(txtDisplay.Text);
+            isOperatorClicked = true;
+        }
+
+        private void Equals_Click(object sender, EventArgs e)
+        {
+            double secondNumber = double.Parse(txtDisplay.Text);
+            double result = 0;
+
+            switch (currentOperator)
             {
-                components.Dispose();
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    if (secondNumber != 0)
+                        result = firstNumber / secondNumber;
+                    else
+                        MessageBox.Show("0-a bölmək olmaz!");
+                    break;
             }
-            base.Dispose(disposing);
+
+            txtDisplay.Text = result.ToString();
+            isOperatorClicked = false;
         }
 
-        #region Windows Form Designer generated code
-
-        private void InitializeComponent()
+        private void Clear_Click(object sender, EventArgs e)
         {
-            label1 = new Label();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            password = new TextBox();
-            button1 = new Button();
-            label3 = new Label();
-            SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(200, 190);
-            label1.Name = "label1";
-            label1.Size = new Size(82, 23);
-            label1.TabIndex = 0;
-            label1.Text = "Username";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.ForeColor = Color.White;
-            label2.Location = new Point(200, 245);
-            label2.Name = "label2";
-            label2.Size = new Size(80, 23);
-            label2.TabIndex = 1;
-            label2.Text = "Password";
-            label2.TextAlign = ContentAlignment.TopCenter;
-            // 
-            // textBox1
-            // 
-            textBox1.BackColor = Color.White;
-            textBox1.BorderStyle = BorderStyle.FixedSingle;
-            textBox1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(310, 185);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(160, 30);
-            textBox1.TabIndex = 2;
-            // 
-            // password
-            // 
-            password.BackColor = Color.White;
-            password.BorderStyle = BorderStyle.FixedSingle;
-            password.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
-            password.Location = new Point(310, 240);
-            password.Name = "password";
-            password.Size = new Size(160, 30);
-            password.TabIndex = 3;
-            password.UseSystemPasswordChar = true;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.FromArgb(46, 204, 113);
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.Font = new Font("Segoe UI", 10F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(320, 300);
-            button1.Name = "button1";
-            button1.Size = new Size(140, 35);
-            button1.TabIndex = 4;
-            button1.Text = "Sign In";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.ForeColor = Color.White;
-            label3.Location = new Point(200, 400);
-            label3.Name = "label3";
-            label3.Size = new Size(0, 20);
-            label3.TabIndex = 5;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(44, 62, 80);
-            ClientSize = new Size(877, 619);
-            Controls.Add(label3);
-            Controls.Add(button1);
-            Controls.Add(password);
-            Controls.Add(textBox1);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Name = "Form1";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Login";
-            ResumeLayout(false);
-            PerformLayout();
+            txtDisplay.Text = "0";
+            firstNumber = 0;
+            currentOperator = "";
         }
-
-        #endregion
-
-        private Label label1;
-        private Label label2;
-        private TextBox textBox1;
-        private TextBox password;
-        private Button button1;
-        private Label label3;
     }
 }
