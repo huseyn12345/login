c949997f8e2e2dc396222b7ce5e2b1705697b20a
huseyn12345 <147710134+huseyn12345@users.noreply.github.com>
2025-10-01 22:10:31 +0400

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,69 +1,101 @@
-using System;
-using System.Windows.Forms;
-
 namespace MPL3
 {
-    public partial class Form1 : Form
+    partial class Form1
     {
-        private string currentOperator = "";
-        private double firstNumber = 0;
-        private bool isOperatorClicked = false;
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.Button btnClear;
 
-        public Form1()
+        protected override void Dispose(bool disposing)
         {
-            InitializeComponent();
-        }
-
-        private void Number_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            if (txtDisplay.Text == "0" || isOperatorClicked)
-                txtDisplay.Text = "";
-            isOperatorClicked = false;
-            txtDisplay.Text += button.Text;
-        }
-
-        private void Operator_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentOperator = button.Text;
-            firstNumber = double.Parse(txtDisplay.Text);
-            isOperatorClicked = true;
-        }
-
-        private void Equals_Click(object sender, EventArgs e)
-        {
-            double secondNumber = double.Parse(txtDisplay.Text);
-            double result = 0;
-
-            switch (currentOperator)
+            if (disposing && (components != null))
             {
-                case "+":
-                    result = firstNumber + secondNumber;
-                    break;
-                case "-":
-                    result = firstNumber - secondNumber;
-                    break;
-                case "*":
-                    result = firstNumber * secondNumber;
-                    break;
-                case "/":
-                    if (secondNumber != 0)
-                        result = firstNumber / secondNumber;
-                    else
-                        MessageBox.Show("0-a bölmək olmaz!");
-                    break;
+                components.Dispose();
             }
-
-            txtDisplay.Text = result.ToString();
-            isOperatorClicked = false;
+            base.Dispose(disposing);
         }
 
-        private void Clear_Click(object sender, EventArgs e)
+        private void InitializeComponent()
         {
-            txtDisplay.Text = "0";
-            firstNumber = 0;
-            currentOperator = "";
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // txtDisplay
+            this.txtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(210, 23);
+            this.txtDisplay.TabIndex = 0;
+            this.txtDisplay.Text = "0";
+            this.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+
+            // btn1
+            this.btn1.Location = new System.Drawing.Point(12, 50);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(45, 45);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Number_Click);
+
+            // btn2
+            this.btn2.Location = new System.Drawing.Point(63, 50);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(45, 45);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Number_Click);
+
+            // btnAdd
+            this.btnAdd.Location = new System.Drawing.Point(114, 50);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(45, 45);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.Operator_Click);
+
+            // btnEquals
+            this.btnEquals.Location = new System.Drawing.Point(165, 50);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(45, 45);
+            this.btnEquals.TabIndex = 4;
+            this.btnEquals.Text = "=";
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.Equals_Click);
+
+            // btnClear
+            this.btnClear.Location = new System.Drawing.Point(12, 101);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(198, 30);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.Clear_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 151);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txtDisplay);
+            this.Name = "Form1";
+            this.Text = "Sadə Kalkulyator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
     }
 }
