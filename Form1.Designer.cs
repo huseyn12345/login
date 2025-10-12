fc275b180a26ce3f88759dde1c268ae8573d5d92
huseyn12345 <147710134+huseyn12345@users.noreply.github.com>
2025-10-12 21:04:25 +0400

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,15 +2,10 @@ namespace Classroom_rezerv
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
+  
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+   
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +17,7 @@ namespace Classroom_rezerv
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
+   
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
@@ -70,21 +62,17 @@ namespace Classroom_rezerv
             groupBox3.SuspendLayout();
             groupBox4.SuspendLayout();
             SuspendLayout();
-            // 
-            // Form1 - modernized visuals
-            // 
+       
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(6, 82, 95); // deep teal background for contrast
+            BackColor = Color.FromArgb(6, 82, 95);
             ClientSize = new Size(720, 680);
             Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
             ForeColor = Color.WhiteSmoke;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Classroom rezerv";
-            // 
-            // groupBox1 (Sinif A) - card style
-            // 
+       
             groupBox1.Controls.Add(button1);
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(textBox2);
@@ -101,10 +89,8 @@ namespace Classroom_rezerv
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Sinif A";
-            groupBox1.BackColor = Color.FromArgb(46, 204, 113); // green card
-            // 
-            // button1
-            // 
+            groupBox1.BackColor = Color.FromArgb(46, 204, 113); 
+          
             button1.BackColor = Color.FromArgb(25, 111, 61);
             button1.Cursor = Cursors.Hand;
             button1.FlatStyle = FlatStyle.Flat;
@@ -120,9 +106,7 @@ namespace Classroom_rezerv
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             button1.ForeColor = Color.White;
-            // 
-            // label3 - remaining time
-            // 
+        
             label3.AutoSize = true;
             label3.Location = new Point(12, 148);
             label3.Name = "label3";
@@ -130,26 +114,20 @@ namespace Classroom_rezerv
             label3.TabIndex = 5;
             label3.Text = "Qalan müddət:";
             label3.ForeColor = Color.WhiteSmoke;
-            // 
-            // textBox2
-            // 
+         
             textBox2.Location = new Point(160, 142);
             textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
             textBox2.ReadOnly = true;
             textBox2.Size = new Size(120, 27);
             textBox2.TabIndex = 4;
-            // 
-            // textBox1 - group code
-            // 
+         
             textBox1.Location = new Point(160, 98);
             textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(120, 27);
             textBox1.TabIndex = 3;
-            // 
-            // maskedTextBox1 - duration
-            // 
+          
             maskedTextBox1.Location = new Point(160, 46);
             maskedTextBox1.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox1.Mask = "00";
@@ -157,9 +135,7 @@ namespace Classroom_rezerv
             maskedTextBox1.Size = new Size(120, 27);
             maskedTextBox1.TabIndex = 2;
             maskedTextBox1.ValidatingType = typeof(int);
-            // 
-            // label2 (Qrup)
-            // 
+         
             label2.AutoSize = true;
             label2.Location = new Point(120, 101);
             label2.Name = "label2";
@@ -167,9 +143,7 @@ namespace Classroom_rezerv
             label2.TabIndex = 1;
             label2.Text = "Qrup:";
             label2.ForeColor = Color.WhiteSmoke;
-            // 
-            // label1 (Müddət)
-            // 
+           
             label1.AutoSize = true;
             label1.Location = new Point(100, 50);
             label1.Name = "label1";
@@ -177,9 +151,7 @@ namespace Classroom_rezerv
             label1.TabIndex = 0;
             label1.Text = "Müddət:";
             label1.ForeColor = Color.WhiteSmoke;
-            // 
-            // groupBox2 (Sinif B) - card style (blue)
-            // 
+          
             groupBox2.Controls.Add(button2);
             groupBox2.Controls.Add(label4);
             groupBox2.Controls.Add(textBox3);
@@ -196,10 +168,8 @@ namespace Classroom_rezerv
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             groupBox2.Text = "Sinif B";
-            groupBox2.BackColor = Color.FromArgb(52, 152, 219); // blue card
-            // 
-            // button2
-            // 
+            groupBox2.BackColor = Color.FromArgb(52, 152, 219); 
+        
             button2.BackColor = Color.FromArgb(18, 88, 137);
             button2.Cursor = Cursors.Hand;
             button2.FlatStyle = FlatStyle.Flat;
@@ -215,9 +185,7 @@ namespace Classroom_rezerv
             button2.UseVisualStyleBackColor = false;
             button2.Click+= button2_Click;
             button2.ForeColor = Color.White;
-            // 
-            // label4
-            // 
+          
             label4.AutoSize = true;
             label4.Location = new Point(12, 148);
             label4.Name = "label4";
@@ -225,26 +193,20 @@ namespace Classroom_rezerv
             label4.TabIndex = 11;
             label4.Text = "Qalan müddət:";
             label4.ForeColor = Color.WhiteSmoke;
-            // 
-            // textBox3
-            // 
+         
             textBox3.Location = new Point(160, 142);
             textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
             textBox3.ReadOnly = true;
             textBox3.Size = new Size(120, 27);
             textBox3.TabIndex = 10;
-            // 
-            // textBox4
-            // 
+         
             textBox4.Location = new Point(160, 98);
             textBox4.Margin = new Padding(3, 4, 3, 4);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(120, 27);
             textBox4.TabIndex = 9;
-            // 
-            // maskedTextBox2
-            // 
+        
             maskedTextBox2.Location = new Point(160, 46);
             maskedTextBox2.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox2.Mask = "00";
@@ -252,9 +214,7 @@ namespace Classroom_rezerv
             maskedTextBox2.Size = new Size(120, 27);
             maskedTextBox2.TabIndex = 8;
             maskedTextBox2.ValidatingType = typeof(int);
-            // 
-            // label5
-            // 
+       
             label5.AutoSize = true;
             label5.Location = new Point(120, 101);
             label5.Name = "label5";
@@ -262,9 +222,7 @@ namespace Classroom_rezerv
             label5.TabIndex = 7;
             label5.Text = "Qrup:";
             label5.ForeColor = Color.WhiteSmoke;
-            // 
-            // label6
-            // 
+        
             label6.AutoSize = true;
             label6.Location = new Point(100, 50);
             label6.Name = "label6";
@@ -272,9 +230,7 @@ namespace Classroom_rezerv
             label6.TabIndex = 6;
             label6.Text = "Müddət:";
             label6.ForeColor = Color.WhiteSmoke;
-            // 
-            // groupBox3 (Sinif C) - purple card
-            // 
+        
             groupBox3.Controls.Add(button3);
             groupBox3.Controls.Add(label7);
             groupBox3.Controls.Add(textBox5);
@@ -291,10 +247,8 @@ namespace Classroom_rezerv
             groupBox3.TabIndex = 2;
             groupBox3.TabStop = false;
             groupBox3.Text = "Sinif C";
-            groupBox3.BackColor = Color.FromArgb(155, 89, 182); // purple
-            // 
-            // button3
-            // 
+            groupBox3.BackColor = Color.FromArgb(155, 89, 182);
+         
             button3.BackColor = Color.FromArgb(113, 54, 131);
             button3.Cursor = Cursors.Hand;
             button3.FlatStyle = FlatStyle.Flat;
@@ -310,9 +264,7 @@ namespace Classroom_rezerv
             button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             button3.ForeColor = Color.White;
-            // 
-            // label7
-            // 
+            
             label7.AutoSize = true;
             label7.Location = new Point(12, 148);
             label7.Name = "label7";
@@ -320,26 +272,20 @@ namespace Classroom_rezerv
             label7.TabIndex = 11;
             label7.Text = "Qalan müddət:";
             label7.ForeColor = Color.WhiteSmoke;
-            // 
-            // textBox5
-            // 
+        
             textBox5.Location = new Point(160, 142);
             textBox5.Margin = new Padding(3, 4, 3, 4);
             textBox5.Name = "textBox5";
             textBox5.ReadOnly = true;
             textBox5.Size = new Size(120, 27);
             textBox5.TabIndex = 10;
-            // 
-            // textBox6
-            // 
+          
             textBox6.Location = new Point(160, 98);
             textBox6.Margin = new Padding(3, 4, 3, 4);
             textBox6.Name = "textBox6";
             textBox6.Size = new Size(120, 27);
             textBox6.TabIndex = 9;
-            // 
-            // maskedTextBox3
-            // 
+          
             maskedTextBox3.Location = new Point(160, 46);
             maskedTextBox3.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox3.Mask = "00";
@@ -347,9 +293,7 @@ namespace Classroom_rezerv
             maskedTextBox3.Size = new Size(120, 27);
             maskedTextBox3.TabIndex = 8;
             maskedTextBox3.ValidatingType = typeof(int);
-            // 
-            // label8
-            // 
+           
             label8.AutoSize = true;
             label8.Location = new Point(120, 101);
             label8.Name = "label8";
@@ -357,9 +301,7 @@ namespace Classroom_rezerv
             label8.TabIndex = 7;
             label8.Text = "Qrup:";
             label8.ForeColor = Color.WhiteSmoke;
-            // 
-            // label9
-            // 
+         
             label9.AutoSize = true;
             label9.Location = new Point(100, 50);
             label9.Name = "label9";
@@ -367,9 +309,7 @@ namespace Classroom_rezerv
             label9.TabIndex = 6;
             label9.Text = "Müddət:";
             label9.ForeColor = Color.WhiteSmoke;
-            // 
-            // groupBox4 (Sinif D) - orange card
-            // 
+            
             groupBox4.Controls.Add(button4);
             groupBox4.Controls.Add(label10);
             groupBox4.Controls.Add(textBox7);
@@ -386,10 +326,8 @@ namespace Classroom_rezerv
             groupBox4.TabIndex = 3;
             groupBox4.TabStop = false;
             groupBox4.Text = "Sinif D";
-            groupBox4.BackColor = Color.FromArgb(243, 156, 18); // orange
-            // 
-            // button4
-            // 
+            groupBox4.BackColor = Color.FromArgb(243, 156, 18);
+         
             button4.BackColor = Color.FromArgb(195, 120, 10);
             button4.Cursor = Cursors.Hand;
             button4.FlatStyle = FlatStyle.Flat;
@@ -405,9 +343,7 @@ namespace Classroom_rezerv
             button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
             button4.ForeColor = Color.White;
-            // 
-            // label10
-            // 
+            
             label10.AutoSize = true;
             label10.Location = new Point(12, 148);
             label10.Name = "label10";
@@ -415,26 +351,20 @@ namespace Classroom_rezerv
             label10.TabIndex = 11;
             label10.Text = "Qalan müddət:";
             label10.ForeColor = Color.WhiteSmoke;
-            // 
-            // textBox7
-            // 
+         
             textBox7.Location = new Point(160, 142);
             textBox7.Margin = new Padding(3, 4, 3, 4);
             textBox7.Name = "textBox7";
             textBox7.ReadOnly = true;
             textBox7.Size = new Size(120, 27);
             textBox7.TabIndex = 10;
-            // 
-            // textBox8
-            // 
+        
             textBox8.Location = new Point(160, 98);
             textBox8.Margin = new Padding(3, 4, 3, 4);
             textBox8.Name = "textBox8";
             textBox8.Size = new Size(120, 27);
             textBox8.TabIndex = 9;
-            // 
-            // maskedTextBox4
-            // 
+         
             maskedTextBox4.Location = new Point(160, 46);
             maskedTextBox4.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox4.Mask = "00";
@@ -442,9 +372,7 @@ namespace Classroom_rezerv
             maskedTextBox4.Size = new Size(120, 27);
             maskedTextBox4.TabIndex = 8;
             maskedTextBox4.ValidatingType = typeof(int);
-            // 
-            // label11
-            // 
+        
             label11.AutoSize = true;
             label11.Location = new Point(120, 101);
             label11.Name = "label11";
@@ -452,9 +380,7 @@ namespace Classroom_rezerv
             label11.TabIndex = 7;
             label11.Text = "Qrup:";
             label11.ForeColor = Color.WhiteSmoke;
-            // 
-            // label12
-            // 
+          
             label12.AutoSize = true;
             label12.Location = new Point(100, 50);
             label12.Name = "label12";
@@ -462,9 +388,7 @@ namespace Classroom_rezerv
             label12.TabIndex = 6;
             label12.Text = "Müddət:";
             label12.ForeColor = Color.WhiteSmoke;
-            // 
-            // add controls to Form
-            // 
+         
             Controls.Add(groupBox4);
             Controls.Add(groupBox3);
             Controls.Add(groupBox2);
