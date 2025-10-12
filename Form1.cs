a43c01c4cc84977e620164bd6ae5d5c8595153ee
huseyn12345 <147710134+huseyn12345@users.noreply.github.com>
2025-10-12 21:02:27 +0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,66 +1,130 @@
 namespace Classroom_rezerv
 {
-public partial class Form1 : Form
-{
-public Form1()
-{
-InitializeComponent();
-}
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            timer2.Interval = 1000;
+            timer3.Interval = 1000;
+            timer4.Interval = 1000;
+        }
+        int counter1 = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text.Equals("")) MessageBox.Show("Zəhmət olmasa müddət daxil edin");
+            else if (textBox1.Text.Equals("")) MessageBox.Show("Zəhmət olmasa qrup kodu daxil edin");
+            else { 
+                timer1.Start();
+            counter1=Convert.ToInt32(maskedTextBox1.Text);
+            textBox2.Text = counter1.ToString();
+            groupBox1.BackColor = Color.Red;
+            timer1.Tick += timer1_Tick;
+        }}
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter1--; 
+            textBox2.Text = counter1.ToString();
 
-private void Form1_Load(object sender, EventArgs e)
-{
-timer1.Interval = 1000;
-timer2.Interval = 1000;
-timer3.Interval = 1000;
-timer4.Interval = 1000;
-}
+            if (counter1 == 0)
+            {
+                timer1.Stop(); 
+                groupBox1.BackColor = Color.Green;
+                textBox1.Clear();
+                maskedTextBox1.Clear();
+                textBox2.Clear();
+            }
+        }
+        int counter2 = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox2.Text.Equals("")) MessageBox.Show("Zəhmət olmasa müddət daxil edin");
+            else if (textBox4.Text.Equals("")) MessageBox.Show("Zəhmət olmasa qrup kodu daxil edin");
+            else
+            {
+                timer2.Start();
+                counter2 = Convert.ToInt32(maskedTextBox2.Text);
+                textBox3.Text = counter2.ToString();
+                groupBox2.BackColor = Color.Red;
+                timer2.Tick += timer2_Tick;
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            counter2--;
+            textBox3.Text = counter2.ToString();
 
+            if (counter2 == 0)
+            {
+                timer2.Stop();
+                groupBox2.BackColor = Color.Green;
+                textBox4.Clear();
+                maskedTextBox2.Clear();
+                textBox3.Clear();
+            }
+        }
+        int counter3 = 0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox3.Text.Equals("")) MessageBox.Show("Zəhmət olmasa müddət daxil edin");
+            else if (textBox6.Text.Equals("")) MessageBox.Show("Zəhmət olmasa qrup kodu daxil edin");
+            else
+            {
+                timer3.Start();
+                counter3 = Convert.ToInt32(maskedTextBox3.Text);
+                textBox5.Text = counter3.ToString();
+                groupBox3.BackColor = Color.Red;
+                timer3.Tick += timer3_Tick;
+            }
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            counter3--;
+            textBox5.Text = counter3.ToString();
 
-int counter1=0,counter2=0,counter3=0,counter4=0;
+            if (counter3 == 0)
+            {
+                timer3.Stop();
+                groupBox3.BackColor = Color.Green;
+                textBox6.Clear();
+                maskedTextBox3.Clear();
+                textBox5.Clear();
+            }
+        }
+        int counter4 = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (maskedTextBox4.Text.Equals("")) MessageBox.Show("Zəhmət olmasa müddət daxil edin");
+            else if (textBox8.Text.Equals("")) MessageBox.Show("Zəhmət olmasa qrup kodu daxil edin");
+            else
+            {
+                timer4.Start();
+                counter4 = Convert.ToInt32(maskedTextBox4.Text);
+                textBox7.Text = counter4.ToString();
+                groupBox4.BackColor = Color.Red;
+                timer4.Tick += timer4_Tick;
+            }
+        }
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            counter4--;
+            textBox7.Text = counter4.ToString();
 
+            if (counter4 == 0)
+            {
+                timer4.Stop();
+                groupBox4.BackColor = Color.Green;
+                textBox8.Clear();
+                maskedTextBox4.Clear();
+                textBox7.Clear();
+            }
+        }
 
-private void button1_Click(object sender, EventArgs e) { StartTimer(maskedTextBox1, textBox1, textBox2, groupBox1, ref counter1, timer1); }
-private void timer1_Tick(object sender, EventArgs e) { TimerTick(ref counter1, textBox2, groupBox1, textBox1, maskedTextBox1); }
-
-
-private void button2_Click(object sender, EventArgs e) { StartTimer(maskedTextBox2, textBox4, textBox3, groupBox2, ref counter2, timer2); }
-private void timer2_Tick(object sender, EventArgs e) { TimerTick(ref counter2, textBox3, groupBox2, textBox4, maskedTextBox2); }
-
-
-private void button3_Click(object sender, EventArgs e) { StartTimer(maskedTextBox3, textBox6, textBox5, groupBox3, ref counter3, timer3); }
-private void timer3_Tick(object sender, EventArgs e) { TimerTick(ref counter3, textBox5, groupBox3, textBox6, maskedTextBox3); }
-
-
-private void button4_Click(object sender, EventArgs e) { StartTimer(maskedTextBox4, textBox8, textBox7, groupBox4, ref counter4, timer4); }
-private void timer4_Tick(object sender, EventArgs e) { TimerTick(ref counter4, textBox7, groupBox4, textBox8, maskedTextBox4); }
-
-
-
-private void StartTimer(MaskedTextBox durationBox, TextBox groupBoxCodeBox, TextBox counterBox, GroupBox group, ref int counter, System.Windows.Forms.Timer timer)
-{
-if(durationBox.Text == "") { MessageBox.Show("Zəhmət olmasa müddət daxil edin"); return; }
-if(groupBoxCodeBox.Text == "") { MessageBox.Show("Zəhmət olmasa qrup kodu daxil edin"); return; }
-counter = Convert.ToInt32(durationBox.Text);
-counterBox.Text = counter.ToString();
-group.BackColor = Color.Red;
-timer.Tick += (s,e) => { TimerTick(ref counter, counterBox, group, groupBoxCodeBox, durationBox); };
-timer.Start();
-}
-
-
-private void TimerTick(ref int counter, TextBox counterBox, GroupBox group, TextBox groupBoxCodeBox, MaskedTextBox durationBox)
-{
-counter--;
-counterBox.Text = counter.ToString();
-if(counter==0)
-{
-var t = group.BackColor;
-counterBox.Clear();
-groupBoxCodeBox.Clear();
-durationBox.Clear();
-group.BackColor = Color.Green;
-}
-}
-}
+    }
 }
