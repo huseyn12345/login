ca81c28c1a936b5cbfe1254c75a017c6f27ae2d8
huseyn12345 <147710134+huseyn12345@users.noreply.github.com>
2025-11-23 23:47:40 +0400

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace AllFileCommand
+﻿namespace samolyot
 {
     partial class Form1
     {
@@ -28,118 +28,199 @@
         /// </summary>
         private void InitializeComponent()
         {
-            richTextBox1 = new RichTextBox();
-            button3 = new Button();
-            button1 = new Button();
-            button4 = new Button();
-            button2 = new Button();
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
             panel1 = new Panel();
-            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
-            // richTextBox1
+            // pictureBox1
             // 
-            richTextBox1.BorderStyle = BorderStyle.None;
-            richTextBox1.Location = new Point(238, 119);
-            richTextBox1.Margin = new Padding(3, 4, 3, 4);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(366, 378);
-            richTextBox1.TabIndex = 0;
-            richTextBox1.Text = "";
-            // 
-            // button3
-            // 
-            button3.Location = new Point(14, 332);
-            button3.Margin = new Padding(3, 4, 3, 4);
-            button3.Name = "button3";
-            button3.Size = new Size(206, 67);
-            button3.TabIndex = 2;
-            button3.Text = "Remove file";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(14, 129);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(206, 67);
-            button1.TabIndex = 3;
-            button1.Text = "Read file";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(14, 434);
-            button4.Margin = new Padding(3, 4, 3, 4);
-            button4.Name = "button4";
-            button4.Size = new Size(206, 63);
-            button4.TabIndex = 4;
-            button4.Text = "Write file";
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += button4_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(14, 231);
-            button2.Margin = new Padding(3, 4, 3, 4);
-            button2.Name = "button2";
-            button2.Size = new Size(206, 70);
-            button2.TabIndex = 5;
-            button2.Text = "Add file";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(110, 402);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 119);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // panel1
             // 
-            panel1.BackColor = Color.FromArgb(255, 128, 0);
-            panel1.Controls.Add(label1);
-            panel1.Location = new Point(1, 0);
+            panel1.BackColor = SystemColors.ActiveBorder;
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(617, 112);
-            panel1.TabIndex = 6;
+            panel1.Size = new Size(379, 543);
+            panel1.TabIndex = 1;
             // 
-            // label1
+            // pictureBox2
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Rockwell Extra Bold", 28F, FontStyle.Bold);
-            label1.Location = new Point(139, 24);
-            label1.Name = "label1";
-            label1.Size = new Size(341, 55);
-            label1.TabIndex = 0;
-            label1.Text = "PAPERMAN";
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(110, 334);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(125, 62);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 128, 0);
+            button1.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(420, 29);
+            button1.Name = "button1";
+            button1.Size = new Size(347, 47);
+            button1.TabIndex = 2;
+            button1.Text = "Eniş";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(255, 128, 0);
+            button2.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(420, 95);
+            button2.Name = "button2";
+            button2.Size = new Size(347, 47);
+            button2.TabIndex = 3;
+            button2.Text = "Turbulans";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 128, 0);
+            button3.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(420, 160);
+            button3.Name = "button3";
+            button3.Size = new Size(347, 47);
+            button3.TabIndex = 4;
+            button3.Text = "Kəmərlər";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(255, 128, 0);
+            button4.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(420, 227);
+            button4.Name = "button4";
+            button4.Size = new Size(347, 47);
+            button4.TabIndex = 5;
+            button4.Text = "Təşəkkürlər";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(255, 128, 0);
+            button5.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Location = new Point(420, 297);
+            button5.Name = "button5";
+            button5.Size = new Size(347, 47);
+            button5.TabIndex = 6;
+            button5.Text = "Beep";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(0, 192, 0);
+            button6.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.ForeColor = SystemColors.ButtonHighlight;
+            button6.Location = new Point(420, 368);
+            button6.Name = "button6";
+            button6.Size = new Size(168, 103);
+            button6.TabIndex = 3;
+            button6.Text = "GAS";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Red;
+            button7.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.ForeColor = SystemColors.ButtonHighlight;
+            button7.Location = new Point(594, 368);
+            button7.Name = "button7";
+            button7.Size = new Size(168, 103);
+            button7.TabIndex = 7;
+            button7.Text = "BREAK";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(0, 0, 192);
+            button8.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.ForeColor = SystemColors.ButtonHighlight;
+            button8.Location = new Point(420, 496);
+            button8.Name = "button8";
+            button8.Size = new Size(347, 47);
+            button8.TabIndex = 8;
+            button8.Text = "ON/OFF";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(192, 64, 0);
-            ClientSize = new Size(616, 529);
-            Controls.Add(panel1);
-            Controls.Add(button2);
+            ClientSize = new Size(800, 543);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
             Controls.Add(button4);
-            Controls.Add(button1);
             Controls.Add(button3);
-            Controls.Add(richTextBox1);
-            Margin = new Padding(3, 4, 3, 4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(panel1);
             Name = "Form1";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "File";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             panel1.ResumeLayout(false);
-            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private RichTextBox richTextBox1;
-        private Button button3;
-        private Button button1;
-        private Button button4;
-        private Button button2;
+        private PictureBox pictureBox1;
         private Panel panel1;
-        private Label label1;
+        private PictureBox pictureBox2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private System.Windows.Forms.Timer timer1;
     }
- }
+}
