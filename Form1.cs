dab01e8d0c20093ec92a2d30907dd7cd9faf109b
huseyn12345 <147710134+huseyn12345@users.noreply.github.com>
2025-11-23 23:47:20 +0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-namespace AllFileCommand
+using System.Media;
+
+namespace samolyot
 {
     public partial class Form1 : Form
     {
@@ -6,57 +8,75 @@ namespace AllFileCommand
         {
             InitializeComponent();
         }
+        SoundPlayer enis = new SoundPlayer(@"C:\Users\mikoq\Downloads\inishazirliklari.wav");
+        SoundPlayer turbulans = new SoundPlayer(@"C:\Users\mikoq\Downloads\turbulans.wav");
+        SoundPlayer tsk = new SoundPlayer(@"C:\Users\mikoq\Downloads\tesekkurler.wav");
+        SoundPlayer beep = new SoundPlayer(@"C:\Users\mikoq\Downloads\beep.wav");
+        SoundPlayer isikacmakapama = new SoundPlayer(@"C:\Users\mikoq\Downloads\isikacmakapamasesi.wav");
+        SoundPlayer kemerler = new SoundPlayer(@"C:\Users\mikoq\Downloads\kemerler.wav");
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            //read file
-            OpenFileDialog op = new OpenFileDialog();
-            op.Filter = "All Files (*.*) | *.*";
-            op.ShowDialog();
-
-            //read
-            richTextBox1.Text = File.ReadAllText(op.FileName);
-
-        }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog op = new OpenFileDialog();
-            op.Filter = "All Files (*.*) | *.*";
-            op.ShowDialog();
-            //append
-            File.AppendAllText(op.FileName, richTextBox1.Text);
+            turbulans.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            enis.Play();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            isikacmakapama.Play();
+            if (pictureBox2.Visible)
+            {
+                pictureBox2.Visible = false;
+            }
+            else pictureBox2.Visible = true;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
-            //Open Dialog
-            OpenFileDialog op = new OpenFileDialog();
-            op.Filter = "All Files (*.*) | *.*";
-            //op.FileName = "document";
-            op.ShowDialog();
-
-            //delete file
-            DialogResult dr = MessageBox.Show("Are you sure to delete this file?", "Delete File", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-            if (dr == DialogResult.Yes)
-            {
-                File.Delete(op.FileName);
-            }
+            kemerler.Play();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            //write file
-            SaveFileDialog sd = new SaveFileDialog();
-            sd.Filter = "All Files (*.*) | *.*";
-            sd.ShowDialog();
-
-            //write
-            File.WriteAllText(sd.FileName, richTextBox1.Text);
-
+            tsk.Play();
         }
 
-   
-    }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            beep.Play();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 10);
+            pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 10);
+            if (pictureBox1.Location.Y <= 0) {
+                timer1.Stop();
+                DialogResult dr = MessageBox.Show("Do you want to play again?", "Restart", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                    Application.Restart();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    } 
 }
