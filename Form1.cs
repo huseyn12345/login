6b4aaaefd00acd0e53da9b9c1e7f81b28416375e
huseyn12345 <147710134+huseyn12345@users.noreply.github.com>
2025-11-27 01:17:21 +0400

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,4 @@
-using System.Media;
-
-namespace samolyot
+namespace card
 {
     public partial class Form1 : Form
     {
@@ -8,75 +6,36 @@ namespace samolyot
         {
             InitializeComponent();
         }
-        SoundPlayer enis = new SoundPlayer(@"C:\Users\mikoq\Downloads\inishazirliklari.wav");
-        SoundPlayer turbulans = new SoundPlayer(@"C:\Users\mikoq\Downloads\turbulans.wav");
-        SoundPlayer tsk = new SoundPlayer(@"C:\Users\mikoq\Downloads\tesekkurler.wav");
-        SoundPlayer beep = new SoundPlayer(@"C:\Users\mikoq\Downloads\beep.wav");
-        SoundPlayer isikacmakapama = new SoundPlayer(@"C:\Users\mikoq\Downloads\isikacmakapamasesi.wav");
-        SoundPlayer kemerler = new SoundPlayer(@"C:\Users\mikoq\Downloads\kemerler.wav");
-
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            turbulans.Play();
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            enis.Play();
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            timer1.Start();
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            timer1.Stop();
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            isikacmakapama.Play();
-            if (pictureBox2.Visible)
+            bool isValid = true;
+            if(textBox1.Text.Length != 16 || !long.TryParse(textBox1.Text, out _))
             {
-                pictureBox2.Visible = false;
+                isValid = false;
             }
-            else pictureBox2.Visible = true;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            kemerler.Play();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            tsk.Play();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            beep.Play();
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 10);
-            pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - 10);
-            if (pictureBox1.Location.Y <= 0) {
-                timer1.Stop();
-                DialogResult dr = MessageBox.Show("Do you want to play again?", "Restart", MessageBoxButtons.YesNo);
-                if (dr == DialogResult.Yes)
-                {
-                    Application.Restart();
-                }
-                else
-                {
-                    Application.Exit();
-                }
+            if(string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                isValid = false;
+            }
+            if(dateTimePicker1.Value < DateTime.Now)
+            {
+                isValid = false;            }
+            if (textBox3.Text.Length != 3 || !int.TryParse(textBox3.Text, out _))
+            {
+                isValid = false;
+            }
+            if(isValid)
+            {
+                MessageBox.Show("Card is valid.");
+                label1.Text = textBox1.Text;
+                label2.Text = textBox2.Text;
+                label3.Text = dateTimePicker1.Value.ToString("MM/yy");
+                label4.Text = textBox3.Text;
+            }
+            else             {
+                MessageBox.Show("Card is invalid.");
             }
         }
-    } 
+    }
 }
