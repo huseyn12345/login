719f7559b0d6ed598ab2606e37a09b1b850fee43
huseyn12345 <147710134+huseyn12345@users.noreply.github.com>
2025-12-11 00:05:42 +0400

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,8 +1,4 @@
-﻿using System.Windows.Forms;
-using System.Xml.Linq;
-using static System.Net.Mime.MediaTypeNames;
-
-namespace card
+﻿namespace imageEdit
 {
     partial class Form1
     {
@@ -32,152 +28,297 @@ namespace card
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panel1 = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            pictureBox2 = new PictureBox();
-            pictureBox1 = new PictureBox();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
             button1 = new Button();
-            dateTimePicker1 = new DateTimePicker();
-            panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            panel8 = new Panel();
+            pictureBox2 = new PictureBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.BackColor = Color.FromArgb(255, 128, 128);
-            panel1.Controls.Add(label1);
-            panel1.Controls.Add(label2);
-            panel1.Controls.Add(label3);
-            panel1.Controls.Add(label4);
-            panel1.Controls.Add(pictureBox2);
-            panel1.Controls.Add(pictureBox1);
-            panel1.Location = new Point(12, 12);
+            panel1.BackColor = Color.Silver;
+            panel1.Location = new Point(0, 89);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(379, 329);
+            panel1.Size = new Size(907, 27);
             panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(192, 16);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(216, 65);
+            button1.TabIndex = 1;
+            button1.Text = "Select image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(66, 13);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(86, 68);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(115, 207);
+            label1.Font = new Font("Stencil", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(679, 33);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
-            label1.TabIndex = 6;
-            label1.Text = "label1";
+            label1.Size = new Size(201, 29);
+            label1.TabIndex = 3;
+            label1.Text = "COLOR FILTERS";
             // 
-            // label2
+            // panel2
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(12, 295);
-            label2.Name = "label2";
-            label2.Size = new Size(50, 20);
-            label2.TabIndex = 7;
-            label2.Text = "label2";
+            panel2.BackColor = Color.Silver;
+            panel2.Location = new Point(637, 199);
+            panel2.Margin = new Padding(3, 4, 3, 4);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(271, 27);
+            panel2.TabIndex = 4;
             // 
-            // label3
+            // panel3
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(226, 295);
-            label3.Name = "label3";
-            label3.Size = new Size(50, 20);
-            label3.TabIndex = 8;
-            label3.Text = "label3";
+            panel3.BackColor = Color.Silver;
+            panel3.Location = new Point(637, 305);
+            panel3.Margin = new Padding(3, 4, 3, 4);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(271, 27);
+            panel3.TabIndex = 5;
             // 
-            // label4
+            // panel4
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(325, 12);
-            label4.Name = "label4";
-            label4.Size = new Size(36, 20);
-            label4.TabIndex = 9;
-            label4.Text = "CVC";
+            panel4.BackColor = Color.Silver;
+            panel4.Location = new Point(637, 415);
+            panel4.Margin = new Padding(3, 4, 3, 4);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(271, 19);
+            panel4.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Silver;
+            panel5.Location = new Point(637, 515);
+            panel5.Margin = new Padding(3, 4, 3, 4);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(271, 27);
+            panel5.TabIndex = 7;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.Silver;
+            panel6.Location = new Point(637, 624);
+            panel6.Margin = new Padding(3, 4, 3, 4);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(271, 23);
+            panel6.TabIndex = 8;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.Silver;
+            panel7.Location = new Point(637, 716);
+            panel7.Margin = new Padding(3, 4, 3, 4);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(271, 20);
+            panel7.TabIndex = 9;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.Silver;
+            panel8.Location = new Point(614, 124);
+            panel8.Margin = new Padding(3, 4, 3, 4);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(23, 612);
+            panel8.TabIndex = 10;
             // 
             // pictureBox2
             // 
-            pictureBox2.Location = new Point(282, 247);
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(135, 173);
+            pictureBox2.Margin = new Padding(3, 4, 3, 4);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(94, 79);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 1;
+            pictureBox2.Size = new Size(345, 420);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 11;
             pictureBox2.TabStop = false;
             // 
-            // pictureBox1
+            // button2
             // 
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(94, 79);
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            button2.BackColor = Color.FromArgb(0, 192, 0);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(135, 628);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(131, 57);
+            button2.TabIndex = 12;
+            button2.Text = "Save image";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
-            // textBox1
+            // button3
             // 
-            textBox1.Location = new Point(421, 32);
-            textBox1.Name = "textBox1";
-            textBox1.PlaceholderText = "Card number";
-            textBox1.Size = new Size(277, 27);
-            textBox1.TabIndex = 1;
+            button3.BackColor = Color.FromArgb(192, 0, 0);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(349, 628);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(131, 57);
+            button3.TabIndex = 13;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
-            // textBox2
+            // button4
             // 
-            textBox2.Location = new Point(421, 99);
-            textBox2.Name = "textBox2";
-            textBox2.PlaceholderText = "Name and surname";
-            textBox2.Size = new Size(277, 27);
-            textBox2.TabIndex = 2;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(664, 124);
+            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Name = "button4";
+            button4.Size = new Size(226, 67);
+            button4.TabIndex = 14;
+            button4.Text = "Demonic";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
-            // textBox3
+            // button5
             // 
-            textBox3.Location = new Point(421, 216);
-            textBox3.Name = "textBox3";
-            textBox3.PlaceholderText = "CVC";
-            textBox3.Size = new Size(277, 27);
-            textBox3.TabIndex = 3;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(664, 233);
+            button5.Margin = new Padding(3, 4, 3, 4);
+            button5.Name = "button5";
+            button5.Size = new Size(226, 67);
+            button5.TabIndex = 15;
+            button5.Text = "Dramatic";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
-            // button1
+            // button6
             // 
-            button1.BackColor = Color.FromArgb(255, 128, 128);
-            button1.Location = new Point(421, 279);
-            button1.Name = "button1";
-            button1.Size = new Size(277, 48);
-            button1.TabIndex = 5;
-            button1.Text = "Card check";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.ForeColor = Color.White;
+            button6.Location = new Point(664, 340);
+            button6.Margin = new Padding(3, 4, 3, 4);
+            button6.Name = "button6";
+            button6.Size = new Size(226, 67);
+            button6.TabIndex = 16;
+            button6.Text = "Freezing";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
-            // dateTimePicker1
+            // button7
             // 
-            dateTimePicker1.Location = new Point(421, 161);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(277, 27);
-            dateTimePicker1.TabIndex = 6;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.ForeColor = Color.White;
+            button7.Location = new Point(664, 441);
+            button7.Margin = new Padding(3, 4, 3, 4);
+            button7.Name = "button7";
+            button7.Size = new Size(226, 67);
+            button7.TabIndex = 17;
+            button7.Text = "Holy";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.ForeColor = Color.White;
+            button8.Location = new Point(664, 549);
+            button8.Margin = new Padding(3, 4, 3, 4);
+            button8.Name = "button8";
+            button8.Size = new Size(226, 67);
+            button8.TabIndex = 18;
+            button8.Text = "Natural";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.ForeColor = Color.White;
+            button9.Location = new Point(664, 655);
+            button9.Margin = new Padding(3, 4, 3, 4);
+            button9.Name = "button9";
+            button9.Size = new Size(226, 53);
+            button9.TabIndex = 19;
+            button9.Text = "Sunny";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 192, 192);
-            ClientSize = new Size(719, 353);
-            Controls.Add(dateTimePicker1);
+            BackColor = Color.FromArgb(0, 0, 192);
+            ClientSize = new Size(904, 737);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(pictureBox2);
+            Controls.Add(panel8);
+            Controls.Add(panel7);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
             Controls.Add(button1);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
             Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Card information";
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -185,16 +326,24 @@ namespace card
         #endregion
 
         private Panel panel1;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
         private Button button1;
+        private PictureBox pictureBox1;
         private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private DateTimePicker dateTimePicker1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private PictureBox pictureBox2;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
     }
-}
+ }
